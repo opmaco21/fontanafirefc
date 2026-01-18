bd2939d4bb8eaa7ea6448191e21b9585a9780c99
opmaco21 <31964431+opmaco21@users.noreply.github.com>
2026-01-18 14:39:12 -0800

    Create faq.js

---
+++ assets/js/faq.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".faq-question").forEach(button => {
+    button.addEventListener("click", () => {
+      const answer = button.nextElementSibling;
+
+      button.classList.toggle("active");
+
+      if (answer.style.display === "block") {
+        answer.style.display = "none";
+      } else {
+        answer.style.display = "block";
+      }
+    });
+  });
+});
